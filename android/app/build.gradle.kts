8d6ca2c282d191878361974edec332207b4c753e
Prashant-ranjan-singh-123 <Prashant.singh.12312345@gmail.com>
2025-10-08 00:40:34 +0530

    1.4.1 update

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 23
         targetSdk = 35
-        versionCode = 32
-        versionName = "1.4.0"
+        versionCode = 33
+        versionName = "1.4.1"
     }
 
     buildTypes {
